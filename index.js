31006e18485a8cbeb88fb36b2935ab5562aeaeda
Jason Galea <jason@lecstor.com>
2017-11-19 21:54:14 +1000

    group commands under 'dev' with config setting - add test validation and completion commands - rename debug and dump commands to use command nesting - update command args config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const chalk = require("chalk");
+
 function setValue(context, prop, value) {
   context.debug && console.log(context, prop, value);
   return context.set(prop, value);
@@ -11,44 +13,79 @@ function getValue(context, prop) {
 
 const commands = {
   set: {
-    command: "set <prop> <value>",
+    args: "prop value",
     description: "Set a context value",
     action: setValue
   },
   get: {
-    command: "get <prop>",
+    args: "prop",
     description: "Get a context value",
     action: getValue
   },
-  dumpStore: {
-    command: "dumpStore <store>",
-    description: "Dump the store",
-    action: async (context, storeId) => {
-      const store = await context.getStore(storeId);
-      console.log(JSON.stringify(await store.read(), null, 2));
+  dump: {
+    store: {
+      args: "storeId",
+      description: "Dump the store",
+      choices: {
+        storeId: ({ context }) => context.listStores()
+      },
+      action: async (context, { storeId }) => {
+        const store = await context.getStore(storeId);
+        console.log(JSON.stringify(await store.read(), null, 2));
+      }
+    },
+    schema: {
+      description: "Dump the schema",
+      action: async context => {
+        console.log(JSON.stringify(context.getSchema(), null, 2));
+      }
     }
   },
-  dumpSchema: {
-    command: "dumpSchema",
-    description: "Dump the schema",
-    action: async context => {
-      console.log(JSON.stringify(context.getSchema(), null, 2));
+  debug: {
+    on: {
+      description: "Turn on debugging",
+      action: async context => {
+        context.debug = true;
+        console.log("Debug On");
+      }
+    },
+    off: {
+      description: "Turn off debugging",
+      action: async context => {
+        context.debug = false;
+        console.log("Debug Off");
+      }
     }
   },
-  debugOn: {
-    description: "Turn on debugging",
-    action: async context => {
-      context.debug = true;
-      console.log("Debug On");
-    }
-  },
-  debugOff: {
-    description: "Turn off debugging",
-    action: async context => {
-      context.debug = false;
-      console.log("Debug Off");
+  test: {
+    validation: {
+      args: "required [optional]",
+      action: async (context, args) => {
+        console.log(`dev test`, { args });
+        if (!args.required) {
+          console.log(chalk.red("Failed: No required arg"));
+        }
+      }
+    },
+    completion: {
+      abcdefg: {
+        action: async () => console.log("abcdefg!!")
+      },
+      abcdefghij: {
+        action: async () => console.log("abcdefghij!!")
+      },
+      abcdefghijklm: {
+        action: async () => console.log("abcdefghijklm!!")
+      },
+      abcabc: {
+        action: async () => console.log("abcabc!!")
+      }
     }
   }
+};
+
+const config = {
+  command: "dev"
 };
 
 /**
@@ -56,7 +93,7 @@ const commands = {
  * @returns {Object} config - plugin config
  */
 function plugin() {
-  return { commands };
+  return { commands, config };
 }
 
 module.exports.plugin = plugin;
